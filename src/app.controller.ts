75ec8640f7c6396124fc389930eb07844f45ad25
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-30 09:43:01 +0700

    Aha

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return { message: this.appService.getHello() };
+  }
+
+  @Get('/error')
+  throwError(@Res() res: Response) {
+    res.status(HttpStatus.NOT_FOUND).json();
   }
 }
